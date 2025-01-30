251ef55ad76de476386bce6fc6f0800606a1cbfe
Ritesh/ <132009069+riteshvish02@users.noreply.github.com>
2025-01-30 17:02:00 +0530

    Update Axios.jsx

--- src/utlis/Axios.jsx
+++ src/utlis/Axios.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
 
-    baseURL: "http://localhost:3000",    
+    baseURL: "https://invoiceback-oy3b.onrender.com",    
    
     withCredentials: true,
   });
